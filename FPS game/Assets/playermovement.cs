d9573508b094385fab19c15975ac1a2acede12c1
MeshachDeveloperTerminalByte <62976486+MeshachDeveloperTerminalByte@users.noreply.github.com>
2020-07-31 12:51:00 +0530

    gane
    uploaded

---
+++ FPS game/Assets/playermovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    private Vector3 velociy=Vector3.zero;
+
+   void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Move(Vector3 _velocity)
+    {
+        velociy = _velocity;
+
+            }
+    void FixedUpdate()
+    {
+       PerformMovement();
+
+    }
+    void PerformMovement()
+    {
+        if(velociy !=Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velociy * Time.fixedDeltaTime);
+        }
+    }
+
+}
---
+++ FPS game/Assets/playermovement1camera.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class playermovement1camera : MonoBehaviour
+{
+    public float mousesen =100f;
+    public Transform player;
+    float xroa = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState =CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousex = Input.GetAxis("Mouse X") * mousesen * Time.deltaTime;
+        float mousey = Input.GetAxis("Mouse Y") * mousesen * Time.deltaTime;
+
+        xroa = Mathf.Clamp(xroa, -80f, 80f);
+        player.Rotate(Vector3.up * mousex);
+        xroa -= mousey;
+        transform.localRotation = Quaternion.Euler(xroa, 0f, 0f);
+
+       
+
+    }
+}
